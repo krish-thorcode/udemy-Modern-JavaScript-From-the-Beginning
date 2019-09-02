c7ed9a5502a1732fe3a5bb2695cb81de43c85fcb
krish-thorcode <krish.thorcode@gmail.com>
2019-09-02 15:44:16 +0530

    done: removeTask, clearTasks, filterTask; todo: persist data on the webpage to localstorage.

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-/* Load all event listeners */
+// Load all event listeners
 loadEventListeners();
 
 function loadEventListeners() {
     // Add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear tasks event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks
+    filter.addEventListener('keyup', filterTasks);
+
 }
 // addTask function
 function addTask(e) {
@@ -36,4 +43,46 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault(); // prevents default form-submit behaviour (ie, do what is in action attribute of form)
+}
+
+// removeTask function
+function removeTask(e) {
+
+    if(e.target.parentElement.classList.contains('delete-item')) {
+
+        if(confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+
+        // console.log(e.target);
+    }
+}
+
+// clearTasks function
+function clearTasks(e) {
+
+    // method 1 - not recommended
+    // taskList.innerHTML = ''
+
+    // method 2 - remove children one by one from the list
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// filterTasks function
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        console.log(task.firstChild);
+        const item = task.firstChild.textContent
+        if(item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+
+    // console.log(text);
 }
